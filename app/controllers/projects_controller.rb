f9756ece6a407f9a6738cb54521080d34d5448bc
Michael Brown <Vurclash@gmail.com>
2019-05-23 14:53:26 -0600

    Added project model, project controller, seeded

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :destroy, :update]
+
   def index
+    @projects = Project.all_projects(current_user.id)
   end
 
   def show
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  belongs_to :user
+
+  def self.all_projects(id)
+    Project.find_by_sql("SELECT * FROM projects AS p WHERE #{id} = p.user_id")
+  end
+
 end
